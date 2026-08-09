f9e2dc1604fc672fc33ec60a92f5211b1af23e6e
sozidatel <sozidatel@users.noreply.github.com>
2026-08-09 20:40:42 +0000

    deploy: cfe108aecf55991a557d6dfd4d8e17ffcca65d43

---
+++ web-1ym3fqDR.js
@@ -0,0 +1,2 @@
+import"./app-t0KQO6Ls.js";import"./stellar-sdk-DPLH474J.js";
+//# sourceMappingURL=web-1ym3fqDR.js.map
--- web-CYP6qp86.js
+++
@@ -1,2 +0,0 @@
-import"./app-B-S95GhN.js";import"./stellar-sdk-DPLH474J.js";
-//# sourceMappingURL=web-CYP6qp86.js.map
